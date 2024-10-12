a100d1d888269200e0de5424d269843acafd720b
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-12 13:29:09 +0200

    Entity passing test

--- inventorymanger/src/main/java/com/example/inventorymanger/model/Item.java
+++ inventorymanger/src/main/java/com/example/inventorymanger/model/Item.java
@@ -5,5 +5,13 @@ public class Item {
     private String name;
     private int quantity;
 
-  
+    public Item(String id, String name, int quantity) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+    }
+    
+    public String getId() { return id; }
+    public String getName() { return name; }
+    public int getQuantity() { return quantity; }
 }
