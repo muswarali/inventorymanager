032bce8bb26cba4eb7b6203f915e16205ba0a9d3
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-17 19:37:42 +0200

    passed find by ID test

--- inventorymanger/src/main/java/com/example/inventorymanager/repository/mongo/ItemMongoRepository.java
+++ inventorymanger/src/main/java/com/example/inventorymanager/repository/mongo/ItemMongoRepository.java
@@ -11,6 +11,7 @@ import com.example.inventorymanger.model.Item;
 import com.example.inventorymanger.repository.ItemRepository;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
 
 public class ItemMongoRepository implements ItemRepository {
 
@@ -33,21 +34,28 @@ public class ItemMongoRepository implements ItemRepository {
 	@Override
 	public List<Item> findAll() {
 		// TODO Auto-generated method stub
-		return StreamSupport
-			    .stream(itemCollection.find().spliterator(), false)
-			    .map(d -> new Item(
-			        "" + d.get("id"),
-			        "" + d.get("name"),
-			        ((Number) d.get("quantity")).intValue(),   // Cast to Number and then to int
-			        ((Number) d.get("price")).doubleValue(),   // Cast to Number and then to double
-			        "" + d.get("description")
-			    ))
-			    .collect(Collectors.toList());
+		return StreamSupport.
+				stream(itemCollection.find().spliterator(), false)
+				.map(this::fromDocumentToItem)
+				.collect(Collectors.toList());
+	}
+	
+	private Item fromDocumentToItem(Document d) {
+		return new Item(
+		        "" + d.get("id"),
+		        "" + d.get("name"),
+		        ((Number) d.get("quantity")).intValue(),   // Cast to Number and then to int
+		        ((Number) d.get("price")).doubleValue(),   // Cast to Number and then to double
+		        "" + d.get("description")
+		    );
 	}
 
 	@Override
 	public Item findById(String id) {
 		// TODO Auto-generated method stub
+		Document d = itemCollection.find(Filters.eq("id", id)).first();
+		if (d != null)
+			return fromDocumentToItem(d);
 		return null;
 	}
 
