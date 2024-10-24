5f388d252d32ccb936595db1f93f87ea64340808
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-24 14:02:49 +0200

    Added App main Method

--- inventorymanger/src/main/java/com/example/inventorymanager/app/swing/InventorySwingApp.java
+++ inventorymanger/src/main/java/com/example/inventorymanager/app/swing/InventorySwingApp.java
@@ -1,5 +1,36 @@
 package com.example.inventorymanager.app.swing;
 
+import java.awt.EventQueue;
+
+import com.example.inventorymanager.repository.mongo.ItemMongoRepository;
+import com.example.inventorymanager.view.swing.InventorySwingView;
+import com.example.inventorymanger.controller.ItemController;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
 public class InventorySwingApp {
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(() -> {
+
+			try {
+				String mongoHost = "localhost";
+				int mongoPort = 27017;
+				if (args.length > 0)
+					mongoHost = args[0];
+				if (args.length > 1)
+					mongoPort = Integer.parseInt(args[1]);
+				ItemMongoRepository itemRepository = new ItemMongoRepository(
+						new MongoClient(new ServerAddress(mongoHost, mongoPort)), "inventory", "student");
+				InventorySwingView itemView = new InventorySwingView();
+				ItemController itemController = new ItemController(itemRepository, itemView);
+				itemView.setItemController(itemController);
+				itemView.setVisible(true);
+				itemController.getAllItems();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+	}
 
 }
