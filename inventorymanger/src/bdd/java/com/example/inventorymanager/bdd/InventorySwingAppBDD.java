1465e60ee6419e7c00288a06c534e2eb86fdc2f2
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-25 19:13:43 +0200

    Added FailOnThreadViolationRepaintManager for EDT

--- inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/InventorySwingAppBDD.java
+++ inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/InventorySwingAppBDD.java
@@ -1,5 +1,7 @@
 package com.example.inventorymanager.bdd;
 
+import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
@@ -10,5 +12,8 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "src/bdd/resources", monochrome = true)
 public class InventorySwingAppBDD {
 	
-
+	@BeforeClass
+	public static void setUpOnce() {
+		FailOnThreadViolationRepaintManager.install();
+	}
 }
