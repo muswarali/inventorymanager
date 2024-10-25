945fc6b3a735ad1c42357879e777a6fcf7b1e9eb
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-25 18:35:00 +0200

    Added initial features which are pending

--- inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/InventorySwingAppBDD.java
+++ inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/InventorySwingAppBDD.java
@@ -1,7 +1,5 @@
 package com.example.inventorymanager.bdd;
 
-import org.assertj.swing.edt.FailOnThreadViolationRepaintManager;
-import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
@@ -12,10 +10,5 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "src/bdd/resources", monochrome = true)
 public class InventorySwingAppBDD {
 	
-	public static int mongoPort =
-			Integer.parseInt(System.getProperty("mongo.port", "27017"));
-	@BeforeClass
-	public static void setUpOnce() {
-		FailOnThreadViolationRepaintManager.install();
-	}
+
 }
---
+++ inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/steps/InventorySwingAppSteps.java
@@ -0,0 +1,26 @@
+package com.example.inventorymanager.bdd.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class InventorySwingAppSteps {
+	@Given("The database contains an item with id {string}, name {string}, quantity {string}, price {string}, and description {string}")
+	public void the_database_contains_an_item_with_id_name_quantity_price_and_description(String string, String string2, String string3, String string4, String string5) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@When("The Inventory View is shown")
+	public void the_Inventory_View_is_shown() {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("The list contains an element with id {string}, name {string}, quantity {string}, price {string}, and description {string}")
+	public void the_list_contains_an_element_with_id_name_quantity_price_and_description(String string, String string2, String string3, String string4, String string5) {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new io.cucumber.java.PendingException();
+	}
+
+}
