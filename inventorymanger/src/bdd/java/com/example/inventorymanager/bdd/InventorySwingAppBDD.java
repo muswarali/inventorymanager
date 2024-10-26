1d3cb5b080445aa9070fae4e673b7c4c0d27b68e
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-26 14:10:59 +0200

    Passing scenario of bdd

--- inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/InventorySwingAppBDD.java
+++ inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/InventorySwingAppBDD.java
@@ -12,6 +12,9 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features = "src/bdd/resources", monochrome = true)
 public class InventorySwingAppBDD {
 	
+	public static int mongoPort =
+			Integer.parseInt(System.getProperty("mongo.port", "27017"));
+	
 	@BeforeClass
 	public static void setUpOnce() {
 		FailOnThreadViolationRepaintManager.install();
