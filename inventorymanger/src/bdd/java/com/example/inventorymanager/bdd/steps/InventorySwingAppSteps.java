8bd791f284803f455e7d54143f71c45238cf7601
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-26 17:32:49 +0200

    Data-tables and step decoupling in BDD

--- inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/steps/InventorySwingAppSteps.java
+++ inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/steps/InventorySwingAppSteps.java
@@ -1,65 +1,50 @@
 package com.example.inventorymanager.bdd.steps;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static com.example.inventorymanager.bdd.steps.DatabaseSteps.COLLECTION_NAME;
+import static com.example.inventorymanager.bdd.steps.DatabaseSteps.DB_NAME;
 
+import java.util.List;
+import java.util.Map;
 
 import javax.swing.JFrame;
 
 import org.assertj.swing.core.BasicRobot;
 import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.core.matcher.JButtonMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
-import org.bson.Document;
 
-import com.mongodb.MongoClient;
+import com.example.inventorymanager.bdd.InventorySwingAppBDD;
 
 import io.cucumber.java.After;
-import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class InventorySwingAppSteps {
 
-	private static final String DB_NAME = "test-db";
-	private static final String COLLECTION_NAME = "test-collection";
 
 	private FrameFixture window;
 
-	private MongoClient mongoClient;
-
-	@Before
-	public void setUp() {
-		mongoClient = new MongoClient();
-		// always start with an empty database
-		mongoClient.getDatabase(DB_NAME).drop();
-	}
-
 	@After
 	public void tearDown() {
-		mongoClient.close();
 		if (window != null)
 			window.cleanUp();
 	}
 
-	@Given("The database contains an item with id {string}, name {string}, quantity {int}, price {double}, and description {string}")
-	public void the_database_contains_an_item_with_id_name_quantity_price_and_description(String id, String name,
-			int quantity, double price, String description) {
-		// Write code here that turns the phrase above into concrete actions
-		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
-				.insertOne(new Document().append("id", id).append("name", name).append("quantity", quantity)
-						.append("price", price).append("description", description));
-
-	}
-
 	@When("The Inventory View is shown")
 	public void the_Inventory_View_is_shown() {
-		// Write code here that turns the phrase above into concrete actions
 		// start the Swing application
 		application("com.example.inventorymanager.app.swing.InventorySwingApp")
-				.withArgs("--db-name=" + DB_NAME, "--db-collection=" + COLLECTION_NAME).start();
+			.withArgs(
+				"--mongo-port=" + InventorySwingAppBDD.mongoPort,
+				"--db-name=" + DB_NAME,
+				"--db-collection=" + COLLECTION_NAME
+			)
+			.start();
 		// get a reference of its JFrame
 		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
 			@Override
@@ -69,11 +54,67 @@ public class InventorySwingAppSteps {
 		}).using(BasicRobot.robotWithCurrentAwtHierarchy());
 	}
 
-	@Then("The list contains an element with id {string}, name {string}, quantity {int}, price {double}, and description {string}")
-	public void the_list_contains_an_element_with_id_name_quantity_price_and_description(String id, String name,
-			int quantity, double price, String description) {
-		// Write code here that turns the phrase above into concrete actions
-		assertThat(window.list().contents()).anySatisfy(e -> assertThat(e).contains(id, name,String.valueOf(quantity),String.valueOf(price),description));
+	@Then("The list contains elements with the following values")
+	public void the_list_contains_elements_with_the_following_values(List<List<String>> values) {
+		values.forEach(
+			v -> assertThat(window.list().contents())
+				.anySatisfy(e -> assertThat(e).contains(v.get(0), v.get(1) , v.get(2) , v.get(3),v.get(4)))
+		);
 	}
+
+	@When("The user enters the following values in the text fields")
+	public void the_user_enters_the_following_values_in_the_text_fields(List<Map<String, String>> values) {
+		values
+			.stream()
+			.flatMap(m -> m.entrySet().stream())
+			.forEach(
+				e -> window
+					.textBox(e.getKey() + "TextBox")
+					.enterText(e.getValue())
+			);
+	}
+
+	@When("The user clicks the {string} button")
+	public void the_user_clicks_the_button(String buttonText) {
+		window.button(JButtonMatcher.withText(buttonText)).click();
+	}
+
+	@Then("An error is shown containing the following values")
+	public void an_error_is_shown_containing_the_following_values(List<List<String>> values) {
+		assertThat(window.label("errorMessageLabel").text())
+			.contains(values.get(0));
+	}
+
+	@Given("The user provides item data in the text fields")
+	public void the_user_provides_item_data_in_the_text_fields() {
+		window.textBox("idTextBox").enterText("1");
+	    window.textBox("nameTextBox").enterText("Laptop");
+	    window.textBox("quantityTextBox").enterText("10");
+	    window.textBox("priceTextBox").enterText("999.99");
+	    window.textBox("descriptionTextBox").enterText("Gaming Laptop");
+	}
+
+	@Then("The list contains the new item")
+	public void the_list_contains_the_new_item() {
+		assertThat(window.list().contents())
+			.anySatisfy(e -> assertThat(e).contains("1", "Laptop" , "10" , "999.9", "Gaming Laptop"));
+	}
+
+	@Given("The user provides item data in the text fields, specifying an existing id")
+	public void the_user_provides_item_data_in_the_text_fields_specifying_an_existing_id() {
+		window.textBox("idTextBox").enterText(DatabaseSteps.ITEM_FIXTURE_1_ID);
+		window.textBox("nameTextBox").enterText("New Laptop");
+	    window.textBox("quantityTextBox").enterText("10");
+	    window.textBox("priceTextBox").enterText("999.99");
+	    window.textBox("descriptionTextBox").enterText("Laptop");
+	}
+
+	@Then("An error is shown containing the name of the existing item")
+	public void an_error_is_shown_containing_the_name_of_the_existing_item() {
+		assertThat(window.label("errorMessageLabel").text())
+			.contains(DatabaseSteps.ITEM_FIXTURE_1_NAME);
+	}
+	
+	
 
 }
--- inventorymanger/src/bdd/resources/item_view.feature
+++ inventorymanger/src/bdd/resources/item_view.feature
@@ -1,7 +1,31 @@
-Feature: Inventory Manager Application Frame
-  Specifications of the behavior of the Inventory Manager Application Frame
+Feature: Inventory View
+  Specifications of the behavior of the Inventory View
 
   Scenario: The initial state of the view
-    Given The database contains an item with id "1", name "Laptop", quantity 10, price 999.9, and description "Simple Laptop"
+    Given The database contains the items with the following values
+      | 1 | Laptop | 10 | 999.9 | Gaming Laptop |
+      | 2 | Mobile |  5 | 599.9 | Smart Phone   |
     When The Inventory View is shown
-    Then The list contains an element with id "1", name "Laptop", quantity 10, price 999.9, and description "Simple Laptop"
+    Then The list contains elements with the following values
+      | 1 | Laptop | 10 | 999.9 | Gaming Laptop |
+      | 2 | Mobile |  5 | 599.9 | Smart Phone   |
+
+  Scenario: Add a new item
+    Given The Inventory View is shown
+    When The user enters the following values in the text fields
+      | id | name   | quantity | price | description   |
+      |  1 | Laptop |        5 | 899.9 | Simple laptop |
+    And The user clicks the "Add Item" button
+    Then The list contains elements with the following values
+      | 1 | Laptop | 5 | 899.9 | Simple laptop |
+
+  Scenario: Add a new item with an existing id
+    Given The database contains the items with the following values
+      | 1 | Laptop | 5 | 899.9 | Simple laptop |
+    And The Inventory View is shown
+    When The user enters the following values in the text fields
+      | id | name   | quantity | price | description   |
+      |  1 | Laptop |        5 | 899.9 | Simple laptop |
+    And The user clicks the "Add Item" button
+    Then An error is shown containing the following values
+      | 1 | Laptop | 5 | 899.9 | Simple laptop |
