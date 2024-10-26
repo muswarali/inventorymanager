635ba28be64cbbce43700e663db5f26afbb2637e
Muswar Ali <149876485+muswarali@users.noreply.github.com>
2024-10-26 13:44:44 +0200

    Initial scenario passed

--- inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/steps/InventorySwingAppSteps.java
+++ inventorymanger/src/bdd/java/com/example/inventorymanager/bdd/steps/InventorySwingAppSteps.java
@@ -1,6 +1,18 @@
 package com.example.inventorymanager.bdd.steps;
 
+import static org.assertj.swing.launcher.ApplicationLauncher.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+import javax.swing.JFrame;
+
+import org.assertj.swing.core.BasicRobot;
+import org.assertj.swing.core.GenericTypeMatcher;
+import org.assertj.swing.finder.WindowFinder;
+import org.assertj.swing.fixture.FrameFixture;
 import org.bson.Document;
+
 import com.mongodb.MongoClient;
 
 import io.cucumber.java.After;
@@ -14,6 +26,8 @@ public class InventorySwingAppSteps {
 	private static final String DB_NAME = "test-db";
 	private static final String COLLECTION_NAME = "test-collection";
 
+	private FrameFixture window;
+
 	private MongoClient mongoClient;
 
 	@Before
@@ -26,32 +40,40 @@ public class InventorySwingAppSteps {
 	@After
 	public void tearDown() {
 		mongoClient.close();
+		if (window != null)
+			window.cleanUp();
 	}
 
-	@Given("The database contains an item with id {string}, name {string}, quantity {string}, price {string}, and description {string}")
+	@Given("The database contains an item with id {string}, name {string}, quantity {int}, price {double}, and description {string}")
 	public void the_database_contains_an_item_with_id_name_quantity_price_and_description(String id, String name,
-			String quantity, String price, String description) {
+			int quantity, double price, String description) {
 		// Write code here that turns the phrase above into concrete actions
 		mongoClient.getDatabase(DB_NAME).getCollection(COLLECTION_NAME)
-				.insertOne(new Document().append("id", id)
-						.append("name", name)
-						.append("quantity", quantity)
-						.append("price", price)
-						.append("description", description));
-		
+				.insertOne(new Document().append("id", id).append("name", name).append("quantity", quantity)
+						.append("price", price).append("description", description));
+
 	}
 
 	@When("The Inventory View is shown")
 	public void the_Inventory_View_is_shown() {
 		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		// start the Swing application
+		application("com.example.inventorymanager.app.swing.InventorySwingApp")
+				.withArgs("--db-name=" + DB_NAME, "--db-collection=" + COLLECTION_NAME).start();
+		// get a reference of its JFrame
+		window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
+			@Override
+			protected boolean isMatching(JFrame frame) {
+				return "Inventory Manager".equals(frame.getTitle()) && frame.isShowing();
+			}
+		}).using(BasicRobot.robotWithCurrentAwtHierarchy());
 	}
 
-	@Then("The list contains an element with id {string}, name {string}, quantity {string}, price {string}, and description {string}")
-	public void the_list_contains_an_element_with_id_name_quantity_price_and_description(String string, String string2,
-			String string3, String string4, String string5) {
+	@Then("The list contains an element with id {string}, name {string}, quantity {int}, price {double}, and description {string}")
+	public void the_list_contains_an_element_with_id_name_quantity_price_and_description(String id, String name,
+			int quantity, double price, String description) {
 		// Write code here that turns the phrase above into concrete actions
-		throw new io.cucumber.java.PendingException();
+		assertThat(window.list().contents()).anySatisfy(e -> assertThat(e).contains(id, name,String.valueOf(quantity),String.valueOf(price),description));
 	}
 
 }
--- inventorymanger/src/bdd/resources/item_view.feature
+++ inventorymanger/src/bdd/resources/item_view.feature
@@ -2,6 +2,6 @@ Feature: Inventory Manager Application Frame
   Specifications of the behavior of the Inventory Manager Application Frame
 
   Scenario: The initial state of the view
-    Given The database contains an item with id "1", name "Laptop", quantity "10", price "999.9", and description "Simple Laptop"
+    Given The database contains an item with id "1", name "Laptop", quantity 10, price 999.9, and description "Simple Laptop"
     When The Inventory View is shown
-    Then The list contains an element with id "1", name "Laptop", quantity "10", price "999.9", and description "Simple Laptop"
+    Then The list contains an element with id "1", name "Laptop", quantity 10, price 999.9, and description "Simple Laptop"
